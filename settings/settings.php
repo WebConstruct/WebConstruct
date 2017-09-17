05a5f74d42d0935bed75b4b9df19e2cf4af559be
James Anderson <james.anderson9182@gmail.com>
2017-09-17 19:41:59 +0100

    this should be created by the user from th example with their own settings

--- settings/settings.php
+++
@@ -1,8 +0,0 @@
-<?php
-$databaseSetting = WebConstruct\Core\Database\DatabaseSettings::singleton();
-
-$databaseSetting->host = 'localhost';
-$databaseSetting->port = 3306;
-$databaseSetting->database = 'testing';
-$databaseSetting->username = 'root';
-$databaseSetting->password = '';
